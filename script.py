1562fb7358d02da0711ab2a8b7643d3d790f43fc
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2020-12-19 21:06:27 +0700

    Mengupload file script.py dan utils.py
    Belajar bersama ProgateID membuat Game Suit Jepang

---
+++ script.py
@@ -0,0 +1,30 @@
+import utils
+# import module random
+import random
+
+print('Memulai permainan Batu Kertas Gunting!')
+player_name = input('Masukkan nama Anda: ')
+if player_name == '':
+    player_name = 'Tamu'
+
+print('Pilih tangan: (0: Batu, 1: Kertas, 2: Gunting)')
+player_hand = int(input('Masukkan nomor (0-2): '))
+
+if utils.validate(player_hand):
+    # Tetapkan nomor acak antara 0 dan 2 ke computer_hand menggunakan randint
+    computer_hand = random.randint(0, 2)
+    
+    utils.print_hand(player_hand, player_name)
+    utils.print_hand(computer_hand, 'Komputer')
+
+    result = utils.judge(player_hand, computer_hand)
+    if result == 'Menang':
+        result = player_name + ' Menang'
+    elif result == 'Kalah':
+        result = player_name + ' kalah'
+    else:
+        result = 'Seri'
+    print('Hasil: ' + result)
+     
+else:
+    print('Mohon masukkan nomor yang benar')
